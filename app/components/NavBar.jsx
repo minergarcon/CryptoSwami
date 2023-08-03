322ca47aa0564e8731ca71646199cd5af80401a2
nikhilpn725 <nikhilpn725@gmail.com>
2023-08-04 00:13:57 +0530

    home page v1 completed

---
+++ app/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='flex flex-row bg-blue-600 justify-center gap-4 cursor-pointer'>
+        <p>Home</p>
+        <p>Bitroot</p>
+        <p>About</p>
+        <Link href="https://twitter.com/cryptoswami69" target='_blank'>
+          Twitter
+        </Link>
+        <p>Contact me</p>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ app/page.js
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import NavBar from "./components/NavBar";
+
+export default function Home() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <div className="bg-green-500 h-screen flex flex-col justify-center items-center gap-4 px-16">
+        <Image
+          src="/bitroot.jpeg"
+          alt="bitroot image"
+          width={100}
+          height={100}
+        ></Image>
+        <h1 className="text-2xl md:text-7xl  md:text-black  font-bold uppercase text-white">
+          Crypto Swami
+        </h1>
+
+        <div className="flex">
+          <p>
+            Crypto Swami is a true visionary in the world of cryptocurrencies.
+            With an innate understanding of blockchain technology and a profound
+            knowledge of the ever-evolving crypto landscape, he stands out as a
+            distinguished figure in the crypto community. As a crypto guy,
+            Crypto Swami's passion for the decentralized revolution is
+            contagious. He has been an early adopter and an enthusiastic
+            advocate of various cryptocurrencies, earning him a reputation as a
+            seasoned investor and advisor. His expertise extends beyond the
+            technical aspects of blockchain technology; he possesses a keen eye
+            for identifying promising projects and trends within the crypto
+            market. Many seek his counsel and insights as they navigate the
+            often complex and volatile world of digital assets.
+          </p>
+          <p>
+            Crypto Swami is a true visionary in the world of cryptocurrencies.
+            With an innate understanding of blockchain technology and a profound
+            knowledge of the ever-evolving crypto landscape, he stands out as a
+            distinguished figure in the crypto community. As a crypto guy,
+            Crypto Swami's passion for the decentralized revolution is
+            contagious. He has been an early adopter and an enthusiastic
+            advocate of various cryptocurrencies, earning him a reputation as a
+            seasoned investor and advisor. His expertise extends beyond the
+            technical aspects of blockchain technology; he possesses a keen eye
+            for identifying promising projects and trends within the crypto
+            market. Many seek his counsel and insights as they navigate the
+            often complex and volatile world of digital assets.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
