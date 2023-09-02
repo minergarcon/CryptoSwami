736c95b5f6ad8455d6aa76f6f4f0898aa0ffdd58
Aju <grgeaju@gmail.com>
2023-09-02 21:24:49 +0530

    navbar completed

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 const NavBar = () => {
   return (
     <div className='flex flex-row bg-white text-black justify-center gap-4 cursor-pointer'>
-        <p>Home</p>
-        <p>Bitroot</p>
-        <Link className='hover:text-blue-500' href="/about"><p>About</p> </Link>
+        <p className='hover:text-blue-400'>Home</p>
+        <p className='hover:text-blue-400'>Bitroot</p>
+        <Link className='hover:text-blue-500 hover:' href="/about"><p>About</p> </Link>
         
         <Link href="https://twitter.com/cryptoswami69" target='_blank'>
           Twitter
--- app/page.js
+++ app/page.js
@@ -1,21 +1,65 @@
+"use client";
 import Image from "next/image";
 import NavBar from "./components/NavBar";
-import { Footer } from "./components/Footer"; 
+import { Footer } from "./components/Footer";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [result, setResult] = useState("Loading.......!!!!!!!");
+  const [bgclr, setbgclr] = useState("bg-green-500");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://api.kanye.rest/");
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        console.log(data, "data from API");
+        setResult(data.quote);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [bgclr]);
+
+  const handlclick = () => {
+    
+    // fetchData();
+    if (bgclr == "bg-green-500") {
+      setbgclr("bg-white");
+    } else {
+      setbgclr("bg-green-500");
+    }
+
+    console.log("Clicked");
+  };
+
   return (
     <>
       <NavBar></NavBar>
-      <div className="bg-green-500 h-screen flex flex-col justify-center items-center gap-4 px-16">
+
+      <div
+        className={`${bgclr} h-screen flex flex-col justify-center items-center gap-4 px-16`}
+      >
         <Image
           src="/bitroot.jpeg"
           alt="bitroot image"
           width={100}
           height={100}
-          ></Image>
+        ></Image>
+        <button onClick={handlclick} className="bg-white px-24  ">
+          Bitroot bot
+        </button>
         <h1 className="text-2xl md:text-7xl  md:text-black  font-bold uppercase text-white">
           Crypto Swami
         </h1>
+        <h2> {result} </h2>
 
         <div className="flex">
           <p>
